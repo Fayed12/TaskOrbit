6149fb97dcc9d0ff5e7da47961082e9b932514b1
mohamed fayed <memad4858@gmail.com>
2025-09-25 02:05:22 +0300

    end home page

--- src/pages/home-page/homePage.jsx
+++ src/pages/home-page/homePage.jsx
@@ -1,13 +1,28 @@
+import { NavLink } from "react-router";
 import style from "./homePage.module.css";
 
 function HomePage() {
   return (
     <div className={style.homePage}>
-      <h1 className={style.siteName}>TaskOrbit</h1>
-      <p className={style.tagline}>
-        Organize your work, boost your productivity, and stay focused with ease
-        🚀
-      </p>
+      <div className={style.message}>
+        <div className={style.nameContainer}>
+          <h1 className={style.siteName}>TaskOrbit</h1>
+        </div>
+        <div className={style.nameContainer}>
+          <p className={style.tagline}>
+            Organize your work, boost your productivity, and stay focused with
+            ease 🚀
+          </p>
+        </div>
+        <NavLink to={"/login"} replace={true}>
+          Start your journey
+        </NavLink>
+      </div>
+
+      <div className={style.image}>
+        <img src="/home-image.webp" alt="home" />
+        <div className={style.feckShadow}></div>
+      </div>
     </div>
   );
 }
