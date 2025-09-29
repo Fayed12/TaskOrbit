6db9e67f9160c52a928838dbac0667a40436894e
mohamed fayed <memad4858@gmail.com>
2025-09-29 19:32:28 +0300

    start add task logic

--- src/components/dashboard-components/home-addTasks/addTasks.jsx
+++ src/components/dashboard-components/home-addTasks/addTasks.jsx
@@ -1,6 +1,33 @@
-import style from "./addTasks.module.css";
+// MUI
+import ClearableProp from "../../dateAddTask";
 
+// react
+import { useState, useEffect } from "react";
+
+// local
+import style from "./addTasks.module.css";
+import MainInput from "../../input"
+
+const initialTaskData = {
+      id: Date.now(),
+      title: "",
+      description: "",
+      completed: false,
+      priority: "",
+      dueDate: null,
+      userId: null,
+}
 function AddTasks({ openAddTask }) {
+  const [newTaskData, setNewTaskData] = useState(initialTaskData);
+  const [userInfoId, setUserInfoId] = useState();
+
+  useEffect(() => {
+    const userInfo = JSON.parse(sessionStorage.getItem("RegisteredUser"));
+    if (userInfo) {
+      setUserInfoId(Number(userInfo.id));
+      setNewTaskData((prev) => ({ ...prev, userId: Number(userInfo.id) }));
+    }
+  },[])
 
   if (!openAddTask) {
     return (
@@ -12,7 +39,66 @@ function AddTasks({ openAddTask }) {
   return (
     <>
       <div className={style.addTasks}>
-        <h1>hello</h1>
+        <div className={style.addHeader}>
+          <h1>Add New Task</h1>
+          <p>Create a new task to add to your list</p>
+        </div>
+        <div className={style.addBody}>
+          <form>
+            <MainInput
+              inpType="text"
+              inpPlaceholder="Enter task title..."
+              inpValue={newTaskData.title}
+              inpSetValue={(e) =>
+                setNewTaskData({
+                  ...newTaskData,
+                  title: e.target.value,
+                })
+              }
+            />
+            <MainInput
+              inpType="text"
+              inpPlaceholder="Add a description (optional)..."
+              inpValue={newTaskData.description}
+              inpSetValue={(e) =>
+                setNewTaskData({
+                  ...newTaskData,
+                  description: e.target.value,
+                })
+              }
+            />
+            <div className={style.specialInput}>
+              <select
+                name="Priority"
+                value={newTaskData.priority}
+                onChange={(e) =>
+                  setNewTaskData({
+                    ...newTaskData,
+                    priority: e.target.value,
+                  })
+                }
+              >
+                <option value="high">High</option>
+                <option value="medium">Medium</option>
+                <option value="low">Low</option>
+              </select>
+              <div>
+                <ClearableProp
+                  newTaskData={newTaskData}
+                  setDateValue={setNewTaskData}
+                />
+              </div>
+            </div>
+            <div className={style.Btn}>
+              <div className={style.successAdd}>
+                <button type="submit">Add Task</button>
+              </div>
+              <div className={style.cancelAdd}>
+                <button type="submit">Cancel</button>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
     </>
   );
---
+++ src/components/dateAddTask.jsx
@@ -0,0 +1,62 @@
+import * as React from "react";
+import { DemoItem } from "@mui/x-date-pickers/internals/demo";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
+import dayjs from "dayjs";
+import Box from "@mui/material/Box";
+import Alert from "@mui/material/Alert";
+
+export default function ClearableProp({ newTaskData, setDateValue }) {
+  const [cleared, setCleared] = React.useState(false);
+
+  React.useEffect(() => {
+    if (cleared) {
+      const timeout = setTimeout(() => {
+        setCleared(false);
+      }, 1500);
+
+      return () => clearTimeout(timeout);
+    }
+    return () => {};
+  }, [cleared]);
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <Box
+        sx={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          justifyContent: "center",
+          position: "relative",
+        }}
+      >
+        <DemoItem label="DesktopDatePicker">
+          <DesktopDatePicker
+            value={newTaskData.dueDate ? dayjs(newTaskData.dueDate) : null}
+            onChange={(newValue) =>
+              setDateValue({
+                ...newTaskData,
+                dueDate: newValue ? newValue.toISOString().split("T").at(0) : null,
+              })
+            }
+            sx={{ width: 260 }}
+            slotProps={{
+              field: { clearable: true, onClear: () => setCleared(true) },
+            }}
+          />
+        </DemoItem>
+
+        {cleared && (
+          <Alert
+            sx={{ position: "absolute", bottom: 0, right: 0 }}
+            severity="success"
+          >
+            Field cleared!
+          </Alert>
+        )}
+      </Box>
+    </LocalizationProvider>
+  );
+}
